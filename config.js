027879aa7b6eb8a823b129e5feb6840fd23a84b8
Hi Im Win <wintrung01@gmail.com>
2023-01-21 05:00:13 +0700

    Update Code

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Trang Anh Cute nha';
+const giftUrl = 'https://www.facebook.com/Lqt2k';
+const eventName = 'Hộp quà hơi xấu sorry';
+const titleCard = 'Tặng Trang Anh Cute';
+const contentCard = 'Chúc Trang Anh Năm Mới Zui Zẻ, Có Nhiều Niềm Zui Ít Nỗi Buồn, Thành Idol Tóp Tóp Triệu Follow Và Đạt Được Nhiều Điều Mình Mong Muốn Nè :3';
+const giftImage = 'TrangAnhCute.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
---
+++ present.js
@@ -0,0 +1,51 @@
+var to = nameGirl;
+var gift_url = giftUrl;
+var gift_image_url = giftImage || giftImageBase64;
+
+var nametag = document.getElementById("nametag");
+var present = document.getElementById("present");
+var presentImage = document.getElementById("present-image");
+
+function init() {
+  var graphElem = document.querySelector('.present-box > .side.top .to');
+  graphElem.setAttribute('data-before', eventName);
+  document.querySelector('#card .title-card').innerHTML = `💘${titleCard}💘`;
+  document.querySelector('#card .content-card').innerHTML = `${contentCard}`;
+  //document.querySelector('#card .end-card').innerHTML = `${EndCardText}`;
+  document.querySelector('#card .honey').setAttribute('src', `${giftImage}`);
+
+  setTimeout(() => alert(' Thích tui thì ấn OK để xem tiếp nha :))))\n Không thích mà xem => Thích á nha :))) '), 1200);
+  setTimeout(() => alert(' Đã thích rồi thì làm người iu của tui đi chứ :))))\n Đồng ý thì ấn OK để xem tiếp nà :))) '), 1200);
+  
+  var _giftLink, 
+      _giftImg;
+  
+  if (gift_url) {
+    _giftLink = document.createElement("a");
+    _giftLink.href = gift_url;
+    _giftLink.target = "_blank";
+    presentImage.appendChild(_giftLink);
+  }
+  
+  if (gift_image_url) {
+    _giftImg = document.createElement("img");
+    _giftImg.src = gift_image_url;
+    if(_giftLink) {
+      _giftLink.appendChild(_giftImg);
+    } else {
+      presentImage.appendChild(_giftImg);
+    }
+  }
+    
+  present.addEventListener("click", function(e){
+    present.classList.toggle("open");
+    document.getElementById('card').classList.add('card-show');
+  }, false);
+  
+  
+  
+  nametag.innerText = to;
+}
+
+init();
+
